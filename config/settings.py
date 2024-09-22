c8b4faedbd9b033f6fc0e824aac6cc99383a595d
N.Dorves <dorves@gmx.net>
2024-09-22 15:12:38 +0200

    'add Django_Project_DZ_7.iml'

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 from environ import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# env = environ.Env()
+# environ.Env.read_env(BASE_DIR / '.env')
 env = Env()
 Env.read_env(BASE_DIR / '.env')
 
@@ -22,7 +25,7 @@ Env.read_env(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG')
--- dz_first_app/models.py
+++ dz_first_app/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('New', 'New'),
+        ('In progress', 'In progress'),
+        ('Pending', 'Pending'),
+        ('Blocked', 'Blocked'),
+        ('Done', 'Done'),
+
+    ]
+    title = models.CharField(max_length=100, unique=True)  # Название задачи.Уникально для даты.
+    description = models.TextField(max_length=300, null=True, blank=True)  # Описание задачи.
+    categories = models.ManyToManyField(Category)  # Категории задачи.Многие ко многим.
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,
+                              default='New')  # Статус задачи.Выбор из:New,In progress,Pending,Blocked,Done
+    deadline = models.DateTimeField()  # Дата и время дедлайн.
+    created_at = models.DateTimeField(auto_now_add=True)  #: Дата/время создания.Авт-тич-е зап-е.
+
+    def __str__(self):
+        return self.title
+
+
+class SubTask(models.Model):
+    STATUS_CHOICES = [
+        ('New', 'New'),
+        ('In progress', 'In progress'),
+        ('Pending', 'Pending'),
+        ('Blocked', 'Blocked'),
+        ('Done', 'Done'),
+
+    ]
+    title = models.CharField(max_length=100)                #Название подзадачи.
+    description = models.TextField()                        # Описание  подзадачи.
+    task = models.ForeignKey(Task, related_name='subtasks', on_delete=models.CASCADE)  # Категории задачи.Многие ко многим.
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,
+                              default='New')                # Статус задачи.Выбор из:New,In progress,Pending,Blocked,Done
+    deadline = models.DateTimeField()                       # Дата/время дедлайн.
+    created_at = models.DateTimeField(auto_now_add=True)    #: Дата/время создания.Авт-тич-е зап-е.
+
+    def __str__(self):
+        return self.title
